e34c716817d3500f02d695b17ae11f6247c57276
Andrew Baughns <abaughns12@gmail.com>
2017-04-26 10:13:19 -0400

    Add basic support for word completions

---
+++ projects/android_studio/JavaLisp/AndroidLispGuiBuilder/src/test/java/com/evolved/automata/android/lisp/guibuilder/CodeCompletionTests.java
@@ -0,0 +1,194 @@
+package com.evolved.automata.android.lisp.guibuilder;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.HashSet;
+import java.util.LinkedList;
+
+/**
+ * Created by Evolved8 on 4/25/17.
+ */
+
+public class CodeCompletionTests {
+
+    static String[] testFunctionNames = new String[]{"mapcar", "map-filter", "setq" , "defun", "switch", "for", "equals", "while", "unless", "break", "signal", "signal-block", "return", "set", "with", "all", "some", "if", "and", "not", "or", "lambda", "integer", "string", "make-int-hashtable", "make-string-hashtable", "get-hash-keys", "funcall", "apply", "defhash", "gethash", "minimal-value-map", "maximum-value-map", "sin", "cos", "tan", "arg-max", "max", "min", "find", "fill-list", "make-range"};
+
+    @Test
+    public void testWordCompletionStorage()
+    {
+        String errorMessage = "Failed to create WordCompletor";
+        try
+        {
+            WordCompletor completer = new WordCompletor();
+            errorMessage = "Failed to add all words";
+
+            HashSet<String> wordMap = new HashSet<String>();
+
+            for (String name:testFunctionNames)
+            {
+                completer.addWord(name);
+                wordMap.add(name);
+            }
+
+            errorMessage = "Failed to get all completions";
+
+            LinkedList<String> completions = completer.getAllWords();
+
+            Assert.assertTrue(errorMessage, completions != null && completions.size() > 0);
+
+
+            errorMessage = "Mismatching data size.  Expected " + testFunctionNames.length + " found: " + completions.size();
+            Assert.assertTrue(errorMessage, testFunctionNames.length == completions.size());
+            errorMessage = "Failed to verify words";
+
+            for (String name:completions)
+            {
+                Assert.assertTrue(errorMessage, wordMap.contains(name));
+            }
+
+
+
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();;
+            Assert.assertTrue(errorMessage, false);
+        }
+    }
+
+    @Test
+    public void testWordCompletion()
+    {
+        String errorMessage = "Failed to create WordCompletor";
+        try
+        {
+            WordCompletor completer = new WordCompletor();
+            errorMessage = "Failed to add all words";
+
+            HashSet<String> wordMap = new HashSet<String>();
+
+            for (String name:testFunctionNames)
+            {
+                completer.addWord(name);
+                wordMap.add(name);
+            }
+
+            errorMessage = "Failed to get all completions";
+
+            LinkedList<String> completions = completer.getAllWords();
+
+            Assert.assertTrue(errorMessage, completions != null && completions.size() > 0);
+
+
+            errorMessage = "Mismatching data size.  Expected " + testFunctionNames.length + " found: " + completions.size();
+            Assert.assertTrue(errorMessage, testFunctionNames.length == completions.size());
+            errorMessage = "Failed to verify words";
+
+            for (String name:completions)
+            {
+                Assert.assertTrue(errorMessage, wordMap.contains(name));
+            }
+
+            String testword = "make-range";
+            int expectedFinalCompletions = 1;
+
+
+            Character ch = null;
+            boolean first = true;
+            for (char c:testword.toCharArray())
+            {
+                ch = Character.valueOf(c);
+                if (first)
+                {
+                    completions = completer.resetCompletionIterator(ch);
+                }
+                else
+                    completions = completer.continueCompletion(ch);
+                first = false;
+                Assert.assertTrue("Completions cannot be null or empty at this point", completions != null && completions.size()>0);
+                System.out.println("Current completions: " + completions);
+            }
+
+            errorMessage = "Incorrect final number of completions: Expected " + expectedFinalCompletions + " but found " + completions.size();
+            Assert.assertTrue(errorMessage, completions.size() == expectedFinalCompletions);
+
+
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();;
+            Assert.assertTrue(errorMessage, false);
+        }
+    }
+
+    @Test
+    public void testWordRemoval()
+    {
+        String errorMessage = "Failed to create WordCompletor";
+        try
+        {
+            WordCompletor completer = new WordCompletor();
+            errorMessage = "Failed to add all words";
+
+            HashSet<String> wordMap = new HashSet<String>();
+
+            for (String name:testFunctionNames)
+            {
+                completer.addWord(name);
+                wordMap.add(name);
+            }
+
+            errorMessage = "Failed to get all completions";
+
+            LinkedList<String> completions = completer.getAllWords();
+
+            Assert.assertTrue(errorMessage, completions != null && completions.size() > 0);
+
+
+            errorMessage = "Mismatching data size.  Expected " + testFunctionNames.length + " found: " + completions.size();
+            Assert.assertTrue(errorMessage, testFunctionNames.length == completions.size());
+
+
+            String wordToRemove = "set";
+            errorMessage = "Failed to attempt to remove word";
+            completer.removeWord(wordToRemove);
+
+            completions = completer.getAllWords();
+
+
+
+            errorMessage = "Failed to verify word removal";
+            for (String name:completions)
+            {
+                errorMessage = "Failed to verify word removal";
+                Assert.assertTrue(errorMessage, !name.equals(wordToRemove));
+                errorMessage = "Failed to verify other words";
+                Assert.assertTrue(errorMessage, wordMap.contains(name));
+            }
+
+            wordToRemove = "setq";
+
+            completer.removeWord(wordToRemove);
+
+            completions = completer.getAllWords();
+
+
+
+            errorMessage = "Failed to verify word removal";
+            for (String name:completions)
+            {
+                errorMessage = "Failed to verify word removal";
+                Assert.assertTrue(errorMessage, !name.equals(wordToRemove));
+                errorMessage = "Failed to verify other words";
+                Assert.assertTrue(errorMessage, wordMap.contains(name));
+            }
+
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();;
+            Assert.assertTrue(errorMessage, false);
+        }
+    }
+}
---
+++ projects/android_studio/JavaLisp/AndroidLispGuiBuilder/src/test/java/com/evolved/automata/android/lisp/guibuilder/WordCompletor.java
@@ -0,0 +1,193 @@
+package com.evolved.automata.android.lisp.guibuilder;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.Stack;
+
+/**
+ * Created by Evolved8 on 4/25/17.
+ */
+
+public class WordCompletor
+{
+    private class Node
+    {
+        HashMap<Character, Node> suffix;
+        String data;
+        public Node()
+        {
+            suffix = new HashMap<Character, Node>();
+        }
+
+        public Node addCharacter(Character c)
+        {
+            Node out = suffix.get(c);
+            if (out == null)
+            {
+                out = new Node();
+                suffix.put(c, out);
+            }
+            return out;
+        }
+
+        public Node addCharacter(Character c, String d)
+        {
+            Node out = suffix.get(c);
+            if (out == null)
+            {
+                out = new Node();
+
+                suffix.put(c, out);
+            }
+            out.setData(d);
+            return out;
+        }
+
+        public Node setData(String d)
+        {
+            data = d;
+            return this;
+        }
+
+        public String getData()
+        {
+            return data;
+        }
+
+        public Node navigateTo(Character c)
+        {
+            return suffix.get(c);
+        }
+
+        public LinkedList<String> getAllCompletions()
+        {
+            LinkedList<String> completions = new LinkedList<String>();
+            for (Character key:suffix.keySet())
+            {
+                Node subtree = suffix.get(key);
+                completions.addAll(subtree.getAllCompletions());
+            }
+            if (data != null)
+                completions.add(data);
+            return completions;
+        }
+
+        public Node remove(Character c)
+        {
+            Node prior = suffix.get(c);
+            if (prior != null)
+                suffix.remove(c);
+            return prior;
+        }
+
+    }
+
+    Node mBase = null, completionIterator;
+
+    public WordCompletor()
+    {
+
+    }
+
+    public LinkedList<String> resetCompletionIterator(Character c)
+    {
+        completionIterator = mBase;
+        return continueCompletion(c);
+
+    }
+
+    public LinkedList<String> getAllWords()
+    {
+        completionIterator = mBase;
+        if (mBase != null)
+            return mBase.getAllCompletions();
+        else
+            return null;
+
+    }
+
+    public LinkedList<String> continueCompletion(Character c)
+    {
+
+        if (completionIterator != null)
+        {
+            completionIterator = completionIterator.navigateTo(c);
+            return completionIterator.getAllCompletions();
+        }
+        else
+            return null;
+
+    }
+
+
+
+    public Node addWord(String word)
+    {
+        if (mBase == null)
+            mBase = new Node();
+        Node cursor = mBase;
+        char c;
+        int i = 0, length = word.length();
+
+        for (i = 0; i < length;i++)
+        {
+            c = word.charAt(i);
+            if (i == length - 1)
+                cursor = cursor.addCharacter(c, word);
+
+            else
+                cursor = cursor.addCharacter(c);
+
+        }
+
+        return mBase;
+    }
+
+    public Node navigatePrefix(String prefix)
+    {
+        Node cursor = mBase;
+
+        for (char c:prefix.toCharArray())
+        {
+            if ((cursor = cursor.navigateTo(c))==null)
+            {
+                break;
+            }
+        }
+        return cursor;
+    }
+
+    public Node removeWord(String word)
+    {
+        Stack<Node> stack = new Stack<Node>();
+
+        Node next, cursor = mBase;
+        Character last = null;
+        for (char c:word.toCharArray())
+        {
+            last = Character.valueOf(c);
+            if ((next = cursor.navigateTo(last))==null)
+            {
+                return null;
+            }
+            stack.push(next);
+            cursor = next;
+        }
+
+        while (stack.size() > 0)
+        {
+            cursor = stack.pop();
+            if (word.equals(cursor.getData()))
+            {
+                cursor.setData(null);
+                next = stack.pop();
+                if (next.getAllCompletions().size() ==0)
+                    next.remove(last);
+                return next;
+            }
+        }
+        return null;
+    }
+
+}
