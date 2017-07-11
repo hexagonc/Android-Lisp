90863133edc78450faa96d811394659d25d6e081
Andrew Baughns <abaughns12@gmail.com>
2017-07-11 12:25:26 -0400

    Starting to add simple tools for processing images

--- projects/android_studio/JavaLisp/common-library/src/main/java/com/evolved/automata/vision/VisionTools.java
+++ projects/android_studio/JavaLisp/common-library/src/main/java/com/evolved/automata/vision/VisionTools.java
@@ -1,6 +1,7 @@
 package com.evolved.automata.vision;
 
 
+import java.nio.ByteBuffer;
 
 /**
  * Created by Evolved8 on 7/5/17.
@@ -11,6 +12,18 @@ public class VisionTools {
     public static String convertDataToBase64(byte[] data)
     {
         return org.apache.commons.codec.binary.Base64.encodeBase64String(data);
+    }
+
+    public static String convertDataToBase64(ByteBuffer buffer)
+    {
+        return convertDataToBase64(buffer.array());
+    }
+
+    public static ByteBuffer convertBase64ToByteBuffer(String base64)
+    {
+        byte[] bytes = org.apache.commons.codec.binary.Base64.decodeBase64(base64);
+        ByteBuffer buffer = ByteBuffer.wrap(bytes);
+        return buffer;
     }
 
 
